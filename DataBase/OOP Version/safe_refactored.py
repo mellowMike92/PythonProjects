735862f2e6fdacbe0db3b9eadac02d9a9ebc8010
Michael F Issa <michael.f.issa@gmail.com>
2019-11-14 16:57:16 -0600

    Update safe_refactored.py

--- DataBase/OOP Version/safe_refactored.py
+++ DataBase/OOP Version/safe_refactored.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import uu
 import sqlite3
@@ -9,11 +10,6 @@ class EncodeDecode:
     """
     Use: Encodes specified file type into binary format for database to store.
     """
-    file_name = ''
-    extension = ''
-    file_path = ''
-    file_string = ''
-    database_file_directory = ''
 
     def __init__(self):
         pass
@@ -25,128 +21,117 @@ class EncodeDecode:
         extension = fn_w_e.split(".")[1]
         return filename, extension
 
-    @classmethod
-    def _process_file_type(cls, filename_with_extension, mode):
-        cls.file_name = cls._strip_filename(filename_with_extension)[0]
-        cls.extension = cls._strip_filename(filename_with_extension)[1]
-        cls.file_path = os.getcwd() + "\\" + filename_with_extension
-        cls.database_file_directory = os.getcwd()
+    def _process_file_type(self, filename_with_extension, mode):
+        self.file_name = self._strip_filename(filename_with_extension)[0]
+        self.extension = self._strip_filename(filename_with_extension)[1]
+        self.file_path = os.getcwd() + "\\" + filename_with_extension
+        self.database_file_directory = os.getcwd()
 
         try:
-            cls.extension = DataBase.file_types[cls.extension]
+            self.extension = DataBase.file_types[self.extension]
         except Exception as e:
             print(e)
 
-        if cls.extension == "IMAGE":
+        if self.extension == "IMAGE":
             if mode == 'encode':
-                cls._encode_image()
+                self._encode_image()
             elif mode == 'decode':
-                cls._decode_image(filename_with_extension)
+                self._decode_image(filename_with_extension)
 
-        elif cls.extension == "TEXT":
+        elif self.extension == "TEXT":
             if mode == 'encode':
-                cls._encode_text()
+                self._encode_text()
             elif mode == 'decode':
-                cls._decode_text(filename_with_extension)
+                self._decode_text(filename_with_extension)
 
-        elif cls.extension == "VIDEO" or cls.extension == "AUDIO":
+        elif self.extension == "VIDEO" or self.extension == "AUDIO":
             if mode == 'encode':
-                cls._encode_video_audio()
+                self._encode_video_audio()
             elif mode == 'decode':
-                cls._decode_video_audio(filename_with_extension)
+                try:
+                    self._decode_video_audio(filename_with_extension)
+                except Exception as e:
+                    exc_type, exc_obj, exc_tb = sys.exc_info()
+                    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                    print(exc_type, fname, exc_tb.tb_lineno)
 
-        cls.extension = cls._strip_filename(filename_with_extension)[1]
+        self.extension = self._strip_filename(filename_with_extension)[1]
 
-    @classmethod
-    def remove_uu_text_file(cls, decoded_filename=None):
-        if decoded_filename is None:
-            encoded_uu_text_file = cls.file_name + ".txt"
+    def remove_uu_text_file(self, file_name=None):
+        if file_name is None:
+            encoded_uu_text_file = self.file_name + ".txt"
             os.remove(encoded_uu_text_file)
         else:
-            os.remove(decoded_filename)
+            os.remove(file_name)
 
-    @classmethod
-    def _encode_video_audio(cls):
-        cls._encode_uu_to_b64()
-        print("File successfully stored!")
+    def _encode_video_audio(self):
+        self._encode_uu_to_b64()
+        print("Audio/Video file successfully stored!")
 
-    @classmethod
-    def _encode_uu_write(cls):
-        uu_file_out = cls.file_name + ".txt"
-        uu_file_encoded_write = uu.encode(cls.file_path, uu_file_out)
+    def _encode_uu_write(self):
+        uu_file_out = self.file_name + ".txt"
+        uu_file_encoded_write = uu.encode(self.file_path, uu_file_out)
         return uu_file_encoded_write
 
-    @classmethod
-    def _encode_uu_read(cls):
-        cls._encode_uu_write()
-        uu_file_in = cls.file_name + ".txt"
-        uu_file_encoded_read = open(uu_file_in, "r").read()
-        return uu_file_encoded_read
-
-    @classmethod
-    def _encode_uu_to_b64(cls):
-        uu_file_read = cls._encode_uu_read()
-        cls.file_string = base64.b64encode(uu_file_read.encode("utf-8"))
-        cls.remove_uu_text_file()
-
-    @classmethod
-    def _decode_video_audio(cls, filename_with_extension):
-        cls._decode_uu_to_b64(filename_with_extension)
-
-    @classmethod
-    def _decode_uu_write(cls):
-        uu_file_out = cls.file_name + ".txt"
-        uu_file_decode_write = uu.encode(cls.file_path, uu_file_out)
-        return uu_file_decode_write
-
-    @classmethod
-    def _decode_uu_to_video_audio(cls, filename_with_extension):
-        filename = filename_with_extension.split(".")[0]
-        uu.decode(filename + ".txt", filename_with_extension)
-        cls.remove_uu_text_file(filename + ".txt")
-        print("File successfully recovered!")
-
-    @classmethod
-    def _decode_uu_to_b64(cls, filename_with_extension):
-        filename = filename_with_extension.split(".")[0]
-        with open(filename + ".txt", 'wb') as f_output:
-            f_output.write(base64.b64decode(cls.file_string))
-        cls._decode_uu_to_video_audio(filename_with_extension)
-
-    @classmethod
-    def _read_text(cls):
-        with open(cls.file_path, "r") as f:
+    def _encode_uu_read(self):
+        self._encode_uu_write()
+        uu_file_in = self.file_name + ".txt"
+        with open(uu_file_in, "r") as f:
             return f.read()
 
-    @classmethod
-    def _encode_text(cls):
-        text = cls._read_text()
-        cls.file_string = base64.b64encode(text.encode("utf-8"))
+    def _encode_uu_to_b64(self):
+        uu_file_read = self._encode_uu_read()
+        self.file_string = base64.b64encode(uu_file_read.encode("utf-8"))
+        self.remove_uu_text_file()
+
+    def _decode_video_audio(self, filename_with_extension):
+        self._decode_uu_to_b64(filename_with_extension)
+
+    def _decode_uu_write(self):
+        uu_file_out = self.file_name + ".txt"
+        uu_file_decode_write = uu.encode(self.file_path, uu_file_out)
+        return uu_file_decode_write
+
+    def _decode_uu_to_video_audio(self, filename_with_extension):
+        filename = filename_with_extension.split(".")[0]
+        uu.decode(filename + ".txt", filename_with_extension)
+        self.remove_uu_text_file(filename + ".txt")
+        print("File {} successfully recovered!".format(filename_with_extension))
+
+    def _decode_uu_to_b64(self, filename_with_extension):
+        filename = filename_with_extension.split(".")[0]
+        with open(filename + ".txt", 'wb') as f_output:
+            f_output.write(base64.b64decode(self.file_string))
+        return self._decode_uu_to_video_audio(filename_with_extension)
+
+    def _read_text(self):
+        with open(self.file_path, "r") as f:
+            return f.read()
+
+    def _encode_text(self):
+        text = self._read_text()
+        self.file_string = base64.b64encode(text.encode("utf-8"))
         # print("file_string\t", cls.file_string)
-        print("File successfully stored!")
-        return cls.file_string
+        print("Text file successfully stored!")
+        return self.file_string
 
-    @classmethod
-    def _decode_text(cls, filename_with_extension):
+    def _decode_text(self, filename_with_extension):
         with open(filename_with_extension, 'wb') as f_output:
-            f_output.write(base64.b64decode(cls.file_string))
+            f_output.write(base64.b64decode(self.file_string))
 
-    @classmethod
-    def _read_image(cls):
-        image_read = cv2.imread(cls.file_path)
+    def _read_image(self):
+        image_read = cv2.imread(self.file_path)
         return image_read
 
-    @classmethod
-    def _encode_image(cls):
-        image = cls._read_image()
-        cls.file_string = base64.b64encode(cv2.imencode('.jpg', image)[1]).decode().encode("utf-8")
-        print("File successfully stored!")
+    def _encode_image(self):
+        image = self._read_image()
+        self.file_string = base64.b64encode(cv2.imencode('.jpg', image)[1]).decode().encode("utf-8")
+        print("Image file successfully stored!")
 
-    @classmethod
-    def _decode_image(cls, filename_with_extension):
+    def _decode_image(self, filename_with_extension):
         with open(filename_with_extension, 'wb') as f_output:
-            f_output.write(base64.b64decode(cls.file_string))
-        print("File successfully recovered!")
+            f_output.write(base64.b64decode(self.file_string))
+        print("File {} successfully recovered!".format(filename_with_extension))
 
 
 class DataBase(EncodeDecode):
@@ -170,6 +155,7 @@ class DataBase(EncodeDecode):
     def __init__(self, sql_file=None, directory=None):
         self.sql_file = sql_file
         self.directory = directory
+        self.file_string = ''
         super().__init__()
 
     @property
@@ -194,7 +180,9 @@ class DataBase(EncodeDecode):
             directory = "current directory"
         print("Creating file: {} in {} \n\nDatabase file created.".format(sql_file, directory))
 
-    def create_database_file(self, sql_file, directory=None):
+    def create_database_file(self, sql_file=None, directory=None):
+        if sql_file is None:
+            sql_file = self.sql_file
         if directory is not None:
             os.chdir(directory)
             self._print_creating_file(sql_file, directory)
@@ -222,7 +210,8 @@ class DataBase(EncodeDecode):
             rows = cursor.fetchall()
             print("Database found!\nSee contents below\n")
             for row in rows:
-                print(row[1] + '\\' + row[0])
+                print('\t' + row[1] + '\\' + row[0])
+            print('\n')
 
     @staticmethod
     def connect_to_database_file(sql_file=None, directory=None):
@@ -241,13 +230,12 @@ class DataBase(EncodeDecode):
         database.execute(store_file_command)
         database.commit()
 
-    @staticmethod
-    def _create_store_file_command():
-        sql_file_name = str(EncodeDecode.file_name)
-        sql_file_extension = str(EncodeDecode.extension)
-        sql_full_filename = str(EncodeDecode.file_name)+"."+str(EncodeDecode.extension)
-        sql_file_string = str(EncodeDecode.file_string)
-        sql_database_file_dir = str(EncodeDecode.database_file_directory)
+    def _create_store_file_command(self):
+        sql_file_name = str(self.file_name)
+        sql_file_extension = str(self.extension)
+        sql_full_filename = str(self.file_name) + "." + str(self.extension)
+        sql_file_string = str(self.file_string, "utf-8")
+        sql_database_file_dir = str(self.database_file_directory)
         store_file_command = 'INSERT or REPLACE INTO SAFE (FILE_NAME, DIRECTORY, NAME, EXTENSION, FILES)' \
                              ' VALUES (%s, %s, %s, %s, %s);' % ('"' + sql_full_filename + '"',
                                                                 '"' + sql_database_file_dir + '"',
@@ -270,16 +258,17 @@ class DataBase(EncodeDecode):
             recovered_files.append(row[1] + "\\" + row[0])
         return recovered_files
 
-    def recover_file(self, recover_file, sql_connection):
-        self._collect_binary_data(recover_file, sql_connection)
-        self._process_file_type(recover_file, "decode")
+    def recover_file(self, recover_file, sql_file):
+        self._collect_binary_data(recover_file, sql_file)
+        self._process_file_type(recover_file, mode="decode")
 
-    @staticmethod
-    def _collect_binary_data(recover_file, sql_connection):
-        database = DataBase.connect_to_database_file(sql_connection)
-        cursor = database.execute("SELECT * from SAFE WHERE FILE_NAME=" + '"' + recover_file + '"')
+    def _collect_binary_data(self, recover_file, sql_file):
+        database = DataBase.connect_to_database_file(sql_file)
+        filename = recover_file.split('\\')[-1]
+        cursor = database.execute("SELECT * from SAFE WHERE FILE_NAME=" + '"' + filename + '"')
+        self.file_string = ''
         for row in cursor:
-            DataBase.file_string += row[4]
+            self.file_string += row[4]
 
     @staticmethod
     def delete_all_recovered_files(delete_all_recovered_confirmation, sql_file):
@@ -290,41 +279,41 @@ class DataBase(EncodeDecode):
                     if name not in protected_files:
                         os.remove(os.path.join(root, name))
 
-    def delete_one_database_file(self, delete_file):
-        database = self.connect_to_database_file()
+    def delete_one_database_file(self, delete_file, sql_file):
+        database = self.connect_to_database_file(sql_file)
         cursor = database.cursor()
         delete_file_command = "DELETE FROM SAFE WHERE FILE_NAME = " + '"' + delete_file + '"'
         cursor.execute(delete_file_command)
         database.commit()
+        print('\n\n...\n\n')
+        self.list_stored_directories(sql_file)
 
-    def delete_all_database_files(self, delete_all_database_files_confirmation):
+    def delete_all_database_files(self, delete_all_database_files_confirmation, sql_file):
         if delete_all_database_files_confirmation.lower() == "y":
-            database = self.connect_to_database_file()
+            database = self.connect_to_database_file(sql_file)
             cursor = database.cursor()
             delete_all_files_command = "DELETE FROM SAFE"
             cursor.execute(delete_all_files_command)
             database.commit()
+            print('\n\n...\n\n')
+            self.list_stored_directories(sql_file)
 
     def list_stored_directories(self, sql_file):
         self.initialize_database_file(sql_file)
-        # cursor = database.cursor()
-        # cursor.execute('SELECT * FROM SAFE')
-        # database.execute('SELECT * FROM SAFE')
-        # rows = database.fetchall()
-        # for row in rows:
-        #     print(row[1] + '\\' + row[0])
 
     def store_file(self, filename_with_extension, file_directory=None, sql_file=None):
         if file_directory is not None:
             os.chdir(file_directory)
         if os.path.exists(filename_with_extension):
-            print("File found!\nNow storing...\n")
-            EncodeDecode._process_file_type(filename_with_extension, "encode")
+            print("File {} found!\nNow storing in {}...\n".format(filename_with_extension, sql_file))
+            self._process_file_type(filename_with_extension, mode="encode")
             self.store_file_in_database(sql_file)
         else:
-            print("File not found!\t{}".format(filename_with_extension))
+            print("File {} not found!\n".format(filename_with_extension))
 
     def store_directory(self, store_directory_file_path, sql_file):
+        protected_files = ["safe.py", sql_file, "DataBase_Shell.py", "safe_refactored.py"]
         for root, dirs, files in os.walk(store_directory_file_path):
             for name in files:
-                self.store_file(name, sql_file=sql_file)
+                if name not in protected_files:
+                    self.store_file(name, sql_file=sql_file)
